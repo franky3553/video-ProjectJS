a6139f6a9cc422d0ff95e1cb34d0fe89080c21b9
franky3553 <franky3553@gmail.com>
2022-07-06 19:27:18 +0200

    Add play and pause event

--- app.js
+++ app.js
@@ -5,13 +5,15 @@
 // selections
 
 const btn = document.querySelector('.switch-btn');
-const video = document.querySelector('.video-contaier');
+const video = document.querySelector('.video-container');
 
 // switch event
 btn.addEventListener('click', function(){
    if (!btn.classList.contains('slide')){
       btn.classList.add('slide');
+      video.pause();
    } else {
       btn.classList.remove('slide');
+      video.play();
    }
 })
